3c958c44ca06a07dabffb63c1c350105a1d56d3c
darkdarkamr <darkdarkamr@hotmail.com>
2024-11-01 22:31:03 +0200

    Update زواج.js

--- plugins/زواج.js
+++ plugins/زواج.js
@@ -1,16 +1,31 @@
-let toM = a => '@' + a.split('@')[0]
+let toM = a => '@' + a.split('@')[0];
+
 function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)}, الف مبروك للعريس🥳✨*
-*${toM(b)}, الف مبروك للعروسه🥳✨*\n\*ملاحظه ده منشن عشوائي للمرح فقط اذ لم يعجبك الأمر إذن ماتستخدمش الامر💗*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
-handler.tags = ['main', 'fun']
-handler.command = ['formarpareja','زواج']
-handler.group = true
-export default handler
+    let ps = groupMetadata.participants.map(v => v.id);
+    let a = ps.getRandom();
+    let b;
+
+    // الحصول على منشن عشوائي آخر يختلف عن الأول
+    do {
+        b = ps.getRandom();
+    } while (b === a);
+
+    // صياغة الرسالة بتعبيرات أكثر جاذبية
+    m.reply(`🌟 *مفاجأة جميلة اليوم!*
+
+🎊 *للعريس: ${toM(a)}، ألف مبروك!* 🥳✨
+🎉 *للعروسة: ${toM(b)}، ألف مبروك! 🎊* 💖
+
+*ملاحظة: هذه مجرد مزحة عشوائية للمرح والفكاهة! إذا لم يعجبك الأمر، يمكنك تجاهلها 💗😊*
+
+*استمتعوا بالجو! 🎈*`, null, {
+        mentions: [a, b]
+    });
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['formarpareja', 'زواج'];
+handler.group = true;
+
+export default handler;
